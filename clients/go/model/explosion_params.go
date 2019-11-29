72da7172c4ae3abcc79c537001af64b595b540e0
prospero78su <prospero.78.su@gmail.com>
2019-11-29 11:02:45 +0300

    024p Change for testing

--- clients/go/model/explosion_params.go
+++ clients/go/model/explosion_params.go
@@ -1,26 +1,35 @@
 package model
 
-import "io"
-import . "aicup2019/stream"
+import (
+	"io"
 
+	mStream "../stream"
+)
+
+//ExplosionParams -- explose with bullet strict games object
 type ExplosionParams struct {
 	Radius float64
 	Damage int32
 }
 
-func NewExplosionParams(radius float64, damage int32) ExplosionParams {
-	return ExplosionParams{
+//NewExplosionParams -- return link to new ExplosionParams
+func NewExplosionParams(radius float64, damage int32) *ExplosionParams {
+	return &ExplosionParams{
 		Radius: radius,
 		Damage: damage,
 	}
 }
-func ReadExplosionParams(reader io.Reader) ExplosionParams {
-	result := ExplosionParams{}
-	result.Radius = ReadFloat64(reader)
-	result.Damage = ReadInt32(reader)
-	return result
+
+//ReadExplosionParams -- read from net ExplosionParams from LocalRunners
+func ReadExplosionParams(reader io.Reader) *ExplosionParams {
+	return &ExplosionParams{
+		Radius: mStream.ReadFloat64(reader),
+		Damage: mStream.ReadInt32(reader),
+	}
 }
+
+//Write -- write ExplosionParams to net to LocalRunner
 func (value ExplosionParams) Write(writer io.Writer) {
-	WriteFloat64(writer, value.Radius)
-	WriteInt32(writer, value.Damage)
+	mStream.WriteFloat64(writer, value.Radius)
+	mStream.WriteInt32(writer, value.Damage)
 }
