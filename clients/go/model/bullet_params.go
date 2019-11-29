784748c172543cbd24c5167dedec411235998587
prospero78su <prospero.78.su@gmail.com>
2019-11-29 09:47:18 +0300

    015p Change import, add comments for exported object, change create object, change strcut to link to struct

--- clients/go/model/bullet_params.go
+++ clients/go/model/bullet_params.go
@@ -1,29 +1,38 @@
 package model
 
-import "io"
-import . "aicup2019/stream"
+import (
+	"io"
+)
 
+//BulletParams -- params for all bullets of andere weapons
 type BulletParams struct {
-    Speed float64
-    Size float64
-    Damage int32
+	Speed  float64
+	Size   float64
+	Damage int32
 }
-func NewBulletParams(speed float64, size float64, damage int32) BulletParams {
-    return BulletParams {
-        Speed: speed,
-        Size: size,
-        Damage: damage,
-    }
+
+//NewBulletParams -- return link to new BulletParams
+func NewBulletParams(speed float64, size float64, damage int32) *BulletParams {
+	return &BulletParams{
+		Speed:  speed,
+		Size:   size,
+		Damage: damage,
+	}
 }
-func ReadBulletParams(reader io.Reader) BulletParams {
-    result := BulletParams {}
-    result.Speed = ReadFloat64(reader)
-    result.Size = ReadFloat64(reader)
-    result.Damage = ReadInt32(reader)
-    return result
+
+//ReadBulletParams -- read BulletParams from net connection from LocalRunner
+func ReadBulletParams(reader io.Reader) *BulletParams {
+	result := &BulletParams{
+		Speed:  ReadFloat64(reader),
+		Size:   ReadFloat64(reader),
+		Damage: ReadInt32(reader),
+	}
+	return result
 }
-func (value BulletParams) Write(writer io.Writer) {
-    WriteFloat64(writer, value.Speed)
-    WriteFloat64(writer, value.Size)
-    WriteInt32(writer, value.Damage)
+
+//Write -- write BulletParams to net connection to LocalRunner
+func (value *BulletParams) Write(writer io.Writer) {
+	WriteFloat64(writer, value.Speed)
+	WriteFloat64(writer, value.Size)
+	WriteInt32(writer, value.Damage)
 }
