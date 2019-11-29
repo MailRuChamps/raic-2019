11f937a7fb67e220fed6ef8c85e30cf407acc38f
prospero78su <prospero.78.su@gmail.com>
2019-11-29 09:18:49 +0300

    009p Formatting, delete import mods and dot, comment exported methods, change struct to link to struct

--- clients/go/my_strategy.go
+++ clients/go/my_strategy.go
@@ -1,19 +1,24 @@
 package main
 
-import . "aicup2019/model"
-import "fmt"
+import (
+	"fmt"
+)
 
-type MyStrategy struct {}
+//MyStrategy -- object to send in LocalRunner
+type MyStrategy struct{}
 
-func NewMyStrategy() MyStrategy {
-	return MyStrategy {}
+//NewMyStrategy -- return link to MyStrategy
+func NewMyStrategy() *MyStrategy {
+	return &MyStrategy{}
 }
 
+//Calculate distance betwin of two games objects
 func distanceSqr(a Vec2Float64, b Vec2Float64) float64 {
-	return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y)
+	return (a.X-b.X)*(a.X-b.X) + (a.Y-b.Y)*(a.Y-b.Y)
 }
 
-func (strategy MyStrategy) getAction(unit Unit, game Game, debug Debug) UnitAction {
+//Return action of MyStrategy for LocalRunner on now tik
+func (strategy *MyStrategy) getAction(unit Unit, game Game, debug Debug) UnitAction {
 	var nearestEnemy *Unit
 	for _, other := range game.Units {
 		if other.PlayerId != unit.PlayerId {
@@ -31,38 +36,38 @@ func (strategy MyStrategy) getAction(unit Unit, game Game, debug Debug) UnitActi
 			}
 		}
 	}
-	targetPos := unit.Position;
+	targetPos := unit.Position
 	if unit.Weapon == nil && nearestWeapon != nil {
 		targetPos = nearestWeapon.Position
 	} else if nearestEnemy != nil {
 		targetPos = nearestEnemy.Position
 	}
-	debug.Draw(CustomDataLog {
+	debug.Draw(CustomDataLog{
 		Text: fmt.Sprintf("Target pos: %v", targetPos),
-	});
-	aim := Vec2Float64 {
+	})
+	aim := Vec2Float64{
 		X: 0,
 		Y: 0,
 	}
 	if nearestEnemy != nil {
-		aim = Vec2Float64 {
+		aim = Vec2Float64{
 			X: nearestEnemy.Position.X - unit.Position.X,
 			Y: nearestEnemy.Position.Y - unit.Position.Y,
 		}
 	}
 	jump := targetPos.Y > unit.Position.Y
-	if targetPos.X > unit.Position.X && game.Level.Tiles[int(unit.Position.X + 1)][int(unit.Position.Y)] == TileWall {
-		jump = true;
+	if targetPos.X > unit.Position.X && game.Level.Tiles[int(unit.Position.X+1)][int(unit.Position.Y)] == TileWall {
+		jump = true
 	}
-	if targetPos.X < unit.Position.X && game.Level.Tiles[int(unit.Position.X - 1)][int(unit.Position.Y)] == TileWall {
-		jump = true;
+	if targetPos.X < unit.Position.X && game.Level.Tiles[int(unit.Position.X-1)][int(unit.Position.Y)] == TileWall {
+		jump = true
 	}
-	return UnitAction {
-		Velocity: targetPos.X - unit.Position.X,
-		Jump: jump,
-		JumpDown: !jump,
-		Aim: aim,
+	return UnitAction{
+		Velocity:   targetPos.X - unit.Position.X,
+		Jump:       jump,
+		JumpDown:   !jump,
+		Aim:        aim,
 		SwapWeapon: false,
-		PlantMine: false,
+		PlantMine:  false,
 	}
 }
