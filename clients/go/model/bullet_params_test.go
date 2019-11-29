21d868ca96b50c019b0c13aaea4f5c579f71ede7
prospero78su <prospero.78.su@gmail.com>
2019-11-29 12:34:36 +0300

    031p Add test for bullet_params (100% coverage)

--- clients/go/model/bullet_params_test.go
+++ clients/go/model/bullet_params_test.go
@@ -4,18 +4,92 @@ package model
 	This module testing of bullets params
 */
 
-import(
+import (
 	mTest "testing"
 )
 
-func TestBulletParams(test *mTest.T){
-	_Posiiv:=func(){
-		_NewBulParam:=func(){
-			if bp:=NewBulletParams(1,2,3);bp==nil{
+type iReader interface {
+	Read()
+}
+type iWriter interface {
+}
+
+type tReader struct {}
+type tWriter struct {}
+
+var (
+	reader *tReader
+	writer *tWriter
+)
+
+func (tr *tReader) Read(pBytes []byte) (int, error) {
+	pBytes = []byte{1, 2, 3}
+	return 3, nil
+}
+
+func (tw *tWriter) Write([]byte) (int, error) {
+	return 3, nil
+}
+
+func TestBulletParams(test *mTest.T) {
+	_Posiiv := func() {
+		_NewBulParam := func() {
+			bp := NewBulletParams(1, 2, 3)
+			if bp == nil {
 				test.Errorf("_NewBulParam(): ERROR bp have nil")
 			}
+			if bp.Speed != 1 {
+				test.Errorf("_NewBulParam(): ERROR in speed(1)=%v", bp.Speed)
+			}
+			if bp.Size != 2 {
+				test.Errorf("_NewBulParam(): ERROR in size(2)=%v", bp.Size)
+			}
+			if bp.Damage != 3 {
+				test.Errorf("_NewBulParam(): ERROR in damage(3)=%v", bp.Damage)
+			}
+
+		}
+		_ReadNet := func() {
+			bp := ReadBulletParams(reader)
+			if bp.Speed != 0 {
+				test.Errorf("_ReadNet(): ERROR in speed(0)=%v", bp.Speed)
+			}
+			if bp.Size != 0 {
+				test.Errorf("_NewBulParam(): ERROR in size(0)=%v", bp.Size)
+			}
+			if bp.Damage != 0 {
+				test.Errorf("_NewBulParam(): ERROR in damage(0)=%v", bp.Damage)
+			}
+		}
+		_WriteNet:=func(){
+			bp := NewBulletParams(1, 2, 3)
+			bp.Write(writer)
 		}
 		_NewBulParam()
+		_ReadNet()
+		_WriteNet()
+
+	}
+	_Negative := func() {
+		_NegativeSize := func() {
+			defer func() {
+				if pan := recover(); pan == nil {
+					test.Errorf("_NegativeSize(): ERROR in panic")
+				}
+			}()
+			NewBulletParams(0, -1, 2)
+		}
+		_NegativeDamage := func() {
+			defer func() {
+				if pan := recover(); pan == nil {
+					test.Errorf("_NegativeDamage(): ERROR in panic")
+				}
+			}()
+			NewBulletParams(1, 1, -1)
+		}
+		_NegativeSize()
+		_NegativeDamage()
 	}
 	_Posiiv()
+	_Negative()
 }
