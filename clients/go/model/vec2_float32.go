3fe8f3ab3bed5470e4068c4ebe3a36d421cf96c1
prospero78su <prospero.78.su@gmail.com>
2019-11-29 10:41:53 +0300

    022p change import, add comment for exported objects, change created, change struct to link t ostruct

--- clients/go/model/vec2_float32.go
+++ clients/go/model/vec2_float32.go
@@ -1,26 +1,33 @@
 package model
 
-import "io"
-import . "aicup2019/stream"
+import (
+	"io"
+)
 
+//Vec2Float32 -- universal struct for geometry
 type Vec2Float32 struct {
 	X float32
 	Y float32
 }
 
-func NewVec2Float32(x float32, y float32) Vec2Float32 {
-	return Vec2Float32{
+//NewVec2Float32 -- return link to new Vec2Float32
+func NewVec2Float32(x float32, y float32) *Vec2Float32 {
+	return &Vec2Float32{
 		X: x,
 		Y: y,
 	}
 }
-func ReadVec2Float32(reader io.Reader) Vec2Float32 {
-	result := Vec2Float32{}
-	result.X = ReadFloat32(reader)
-	result.Y = ReadFloat32(reader)
-	return result
+
+//ReadVec2Float32 -- read Vec2Float32 from net from LocalRunner
+func ReadVec2Float32(reader io.Reader) *Vec2Float32 {
+	return &Vec2Float32{
+		X: ReadFloat32(reader),
+		Y: ReadFloat32(reader),
+	}
 }
-func (value Vec2Float32) Write(writer io.Writer) {
+
+//Write -- write Vec2Float32 to net to LocalRunner
+func (value *Vec2Float32) Write(writer io.Writer) {
 	WriteFloat32(writer, value.X)
 	WriteFloat32(writer, value.Y)
 }
