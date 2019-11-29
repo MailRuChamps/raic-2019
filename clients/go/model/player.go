63da5e20c58f7bb0a875c3d04704c298cad18add
prospero78su <prospero.78.su@gmail.com>
2019-11-29 09:41:17 +0300

    014p Formatting, change import, add comment to exported objects, change struct to link to struct

--- clients/go/model/player.go
+++ clients/go/model/player.go
@@ -1,25 +1,33 @@
 package model
 
-import "io"
-import . "aicup2019/stream"
+import (
+	"io"
+)
 
+//Player -- object of player on game field
 type Player struct {
-    Id int32
-    Score int32
+	Id    int32
+	Score int32
 }
-func NewPlayer(id int32, score int32) Player {
-    return Player {
-        Id: id,
-        Score: score,
-    }
+
+//NewPlayer -- retun link to new Player
+func NewPlayer(id int32, score int32) *Player {
+	return &Player{
+		Id:    id,
+		Score: score,
+	}
 }
-func ReadPlayer(reader io.Reader) Player {
-    result := Player {}
-    result.Id = ReadInt32(reader)
-    result.Score = ReadInt32(reader)
-    return result
+
+//ReadPlayer -- read Player from net connection from LocalRunner
+func ReadPlayer(reader io.Reader) *Player {
+	result := &Player{}
+	result.Id = ReadInt32(reader)
+	result.Score = ReadInt32(reader)
+	return result
 }
-func (value Player) Write(writer io.Writer) {
-    WriteInt32(writer, value.Id)
-    WriteInt32(writer, value.Score)
+
+//Write -- write Player to net connection to LocalRunner
+func (value *Player) Write(writer io.Writer) {
+	WriteInt32(writer, value.Id)
+	WriteInt32(writer, value.Score)
 }
