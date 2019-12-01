0bce1fc237fb0ba7cf23dfc1e2d23c40529a2ecc
Unknown <alone_monax@xakep.ru>
2019-12-01 06:43:28 +0300

    dockerfile and compile.sh from 2018's cup

--- clients/javascript/compile.sh
+++ clients/javascript/compile.sh
@@ -1,14 +1,13 @@
+if [ "$1" == "" ]; then
+    echo This script is used for compiling on the server
+    exit 1
+fi
+
 set -ex
 
 if [ "$1" != "base" ]; then
-    if [[ `ls -1 /src/ | wc -l` -eq 1 ]]; then
-        cp -f /src/my_strategy.py my_strategy.py
-    else
-        rm -rf ./*
-        cp -rf /src/* ./
-    fi
+    rm "my-strategy.js"
+    cp -rn /src/* ./
 fi
 
-find . -name '*.pyx' -exec cythonize -i {} \;
-python -m py_compile main.py
 cp -r * /output/
